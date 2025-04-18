6d60c2b623f9b3a9dfc1a6bd72e9d535ed79d5e1
diogosantosgoncalves <diogo_santos_goncalves@hotmail.com>
2025-04-18 19:15:16 -0300

    Adicionar arquivos de projeto.

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,42 @@
+using Ocelot.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config
+        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName}.json")
+        .AddEnvironmentVariables();
+});
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Api Gateway funcionando");
+    });
+});
+
+app.Run();
