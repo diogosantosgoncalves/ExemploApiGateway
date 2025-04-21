29e47ba8527f9a593abf09e0221071290d6b3b1e
diogosantosgoncalves <diogo_santos_goncalves@hotmail.com>
2025-04-21 17:00:25 -0300

    reorganizado configuração addJsonFile configuration.Development.json

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -3,15 +3,7 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
-{
-    config
-        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
-        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-        .AddJsonFile($"configuration.{hostingContext.HostingEnvironment.EnvironmentName}.json")
-        .AddEnvironmentVariables();
-});
+builder.Configuration.AddJsonFile("configuration.Development.json");
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
